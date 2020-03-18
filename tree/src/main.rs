b32ce13aa45e32e6b5473a7ae44a9897c30e6af0
unknown <jfeldman@colgate.edu>
2020-03-18 10:15:50 -0400

    finished pointer chapter including traversable tree

--- tree/src/main.rs
+++ tree/src/main.rs
@@ -1,3 +1,46 @@
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>,
+    parent: RefCell<Weak<Node>>,
+}
+
+impl Node {
+    fn new(value: i32, children: RefCell<Vec<Rc<Node>>>, parent: RefCell<Weak<Node>>) -> Node {
+        Node {
+            value,
+            children,
+            parent,
+        }
+    }
+}
+
+impl Drop for Node {
+    fn drop(&mut self) {
+        println!("Node with value {} went out of scope", self.value)
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let leaf = Rc::new(Node::new(
+        3,
+        RefCell::new(vec![]),
+        RefCell::new(Weak::new()),
+    ));
+    let root = Rc::new(Node::new(
+        6,
+        RefCell::new(vec![Rc::clone(&leaf)]),
+        RefCell::new(Weak::new()),
+    ));
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&root);
+    println!(
+        "{:?}",
+        match leaf.parent.borrow().upgrade() {
+            Some(x) => x.value,
+            None => -1,
+        }
+    );
 }
